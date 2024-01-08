47771a88f2102a803612f6e92ef3dea3c4486f28
uyvwf <uyvwf@student.kit.edu>
2024-01-08 20:54:23 +0100

    first work on tracks.py Added:
    Added tests:
    
    changed:
    different conzept for railbuilding. No curvechecks

--- src/model/tracks.py
+++ src/model/tracks.py
@@ -1,9 +1,19 @@
 import pygame
+from view_mode import View_mode
 
 class Track:
     def __init__(self, pieces):
         self.pieces = pieces
 
+
+class Switch:
+
+    def __init__(self, position):
+        self.pos = position
+
+
+    def draw(self, surface, view_mode):
+        pass
 
 
 class Incomplete_track:
@@ -11,16 +21,6 @@ class Incomplete_track:
         self.pieces = []
         self.start_switch = start_switch
         self.end_switch = end_switch
-
-    def check_track(self):
-        is_straight = False
-        for piece in self.pieces:
-            if is_straight and type(piece) is Straight_piece:
-                return False
-            if not is_straight and type(piece) is Curved_piece:
-                return False
-            is_straight = not is_straight
-        return True
 
     def add_piece(self, piece):
         self.pieces.append(piece)
@@ -31,29 +31,14 @@ class Incomplete_track:
     def get_complete_track(self):
         if len(self.pieces) == 0:
             raise Exception("Track can't be completed, because it has no pieces")
-        if not self.check_track():
-            raise Exception("Track can't be completed, because self.check_track doesnt return True")
         if self.start_switch.position != self.pieces[0].point1:
             raise Exception("Track can't be completed, first piece doesnt start at starting_switch")
         if self.end_switch.position != self.pieces[-1].point2:
             raise Exception("Track can't be completed, last piece doesnt end at end_switch")
 
 
-
-class Straight_piece:
+class Piece:
     def __init__(self, point1, point2):
         self.point1 = point1
         self.point2 = point2
-        self.max_speed = float("inf")
-
-
-class Curved_piece:
-    def __init__(self, point1, point2, curve_point):
-        self.point1 = point1
-        self.point2 = point2
-        self.curve_point = curve_point
-        self.max_speed = self.__get_max_speed()
-
-    def __get_max_speed(self):
-        # TODO
-        return .0
+        self.maxSpeed = 0
---
+++ src/model/view_mode.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class View_mode(Enum):
+    MAIN = 1
